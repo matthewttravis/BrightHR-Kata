796ba7f0a81926dc11c9ace0c073440eba027103
Matt Travis <matthew.travis@azenta.com>
2022-05-09 23:43:09 +0100

    Refactored GetTotalPrice method by introducing the Special class to hold the special rules, and the ProcessPrice method to find the total price of a item group.

--- ShoppingCartKata/ShoppingCartKata/Checkout.cs
+++ ShoppingCartKata/ShoppingCartKata/Checkout.cs
@@ -3,6 +3,7 @@
     public class Checkout: ICheckout
     {
         private readonly Dictionary<string, int> _priceList;
+        private readonly List<Special> _specials;
         private readonly List<string> _shoppingCart = new List<string>();
 
         public Checkout()
@@ -13,6 +14,12 @@
                 { "B", 30 },
                 { "C", 20 },
                 { "D", 15 }
+            };
+
+            _specials = new List<Special>
+            {
+                new Special("A", 3, 130),
+                new Special("B", 2, 45)
             };
         }
 
@@ -29,35 +36,31 @@
 
             foreach (var itemGroup in groups)
             {
-                var itemCount = itemGroup.Count();
-                switch (itemGroup.Key)
-                {
-                    case "A":
-                        while (itemCount >= 3)
-                        {
-                            itemCount -= 3;
-                            total += 130;
-                        }
-
-                        total += itemCount * 50;
-                        break;
-                    case "B":
-                        while (itemCount >= 2)
-                        {
-                            itemCount -= 2;
-                            total += 45;
-                        }
-
-                        total += itemCount * 30;
-                        break;
-                    default:
-                        total += _priceList[itemGroup.Key];
-                        break;
-                }
+                total += ProcessPrice(itemGroup);
             }
 
             return total;
         }
 
+        private int ProcessPrice(IGrouping<string, string> itemGroup)
+        {
+            var total = 0;
+            var count = itemGroup.Count();
+
+            var rule = _specials.SingleOrDefault(x => x.Sku == itemGroup.Key);
+
+            if (rule != null)
+            {
+                while (count >= rule.Count)
+                {
+                    count -= rule.Count;
+                    total += rule.Price;
+                }
+            }
+            
+            total += _priceList[itemGroup.Key] * count;
+
+            return total;
+        }
     }
 }
---
+++ ShoppingCartKata/ShoppingCartKata/Special.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingCartKata;
+
+public class Special
+{
+    public string Sku {get; set;}
+    public int Count {get; set;}
+    public int Price {get; set;}
+
+    public Special(string sku, int count, int price)
+    {
+        Sku = sku;
+        Count = count;
+        Price = price;
+    }
+}
