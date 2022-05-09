c340164a4e3c68f933db652c1c7ca2c6a56a0ad0
Matt Travis <matthew.travis@azenta.com>
2022-05-09 22:47:54 +0100

    Updated GetTotalPrice method to group items by SKU, then for group "A", apply the special price if there are enough items in a decreasing loop until there are not enough items to apply the rule satisfying the latest test and the "A" special offer.

--- ShoppingCartKata/ShoppingCartKata/Checkout.cs
+++ ShoppingCartKata/ShoppingCartKata/Checkout.cs
@@ -2,9 +2,8 @@
 {
     public class Checkout: ICheckout
     {
-        private int _total;
-
         private readonly Dictionary<string, int> _priceList;
+        private readonly List<string> _shoppingCart = new List<string>();
 
         public Checkout()
         {
@@ -19,12 +18,36 @@
 
         public void Scan(string item)
         {
-            _total += _priceList[item];
+            _shoppingCart.Add(item);
         }
 
         public int GetTotalPrice()
         {
-            return _total;
+            var total = 0;
+
+            var groups = _shoppingCart.GroupBy(x => x);
+
+            foreach (var itemGroup in groups)
+            {
+                var itemCount = itemGroup.Count();
+                if (itemGroup.Key == "A")
+                {
+                    while (itemCount >= 3)
+                    {
+                        itemCount -= 3;
+                        total += 130;
+                    }
+
+                    total += itemCount * 50;
+                }
+                else
+                {
+                    total += _priceList[itemGroup.Key];
+                }
+            }
+
+            return total;
         }
+
     }
 }
