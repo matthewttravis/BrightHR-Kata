62954b39984e744d71defcb3a467afd70019c75d
Matt Travis <matthew.travis@azenta.com>
2022-05-09 19:21:59 +0100

    Extended tests to cover all base item prices.

--- ShoppingCartKata/ShoppingCartKata/ShoppingCartTests.cs
+++ ShoppingCartKata/ShoppingCartKata/ShoppingCartTests.cs
@@ -6,7 +6,7 @@ namespace ShoppingCartKata
     public class ShoppingCartTests
     {
         [Test]
-        public void CanScanItem()
+        public void CanScanItemA()
         {
             var checkout = new Checkout();
             checkout.Scan("A");
@@ -14,6 +14,39 @@ namespace ShoppingCartKata
             var total = checkout.GetTotalPrice();
 
             Assert.AreEqual(total, 50);
+        }
+        
+        [Test]
+        public void CanScanItemB()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("B");
+
+            var total = checkout.GetTotalPrice();
+
+            Assert.AreEqual(total, 30);
+        }
+
+        [Test]
+        public void CanScanItemC()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("C");
+
+            var total = checkout.GetTotalPrice();
+
+            Assert.AreEqual(total, 20);
+        }
+
+        [Test]
+        public void CanScanItemD()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("D");
+
+            var total = checkout.GetTotalPrice();
+
+            Assert.AreEqual(total, 15);
         }
     }
 }
